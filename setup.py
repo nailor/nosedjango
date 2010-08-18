cd4ecc6819347c84429d7758370d9e472ef5a154
Jyrki Pulliainen <jyrki@dywypi.org>
2010-08-18 20:12:31 +0300

    setup.py: Do a minor style fix

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ setup(
     name='NoseDjango',
     version='.'.join(str(x) for x in __version__),
     author='Jyrki Pulliainen',
-    author_email = 'jyrki.pulliainen@inoi.fi',
-    description = 'nose plugin for easy testing of django projects ' \
+    author_email='jyrki.pulliainen@inoi.fi',
+    description='nose plugin for easy testing of django projects ' \
         'and apps. Sets up a test database (or schema) and installs apps ' \
         'from test settings file before tests are run, and tears the test ' \
         'database (or schema) down after all tests are run.',
     install_requires='nose>=0.10',
-    url = "http://www.assembla.com/spaces/nosedjango",
-    license = 'GNU LGPL',
-    packages = find_packages(),
-    zip_safe = False,
-    include_package_data = True,
-    entry_points = {
+    url="http://www.assembla.com/spaces/nosedjango",
+    license='GNU LGPL',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    entry_points={
         'nose.plugins': [
-            'django = nosedjango.nosedjango:NoseDjango',
+            'django=nosedjango.nosedjango:NoseDjango',
             ]
         }
     )
